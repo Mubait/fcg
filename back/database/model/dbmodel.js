91e917e20cd1ab8072e9d9a5dea8f6b0affd2557
Mubait <kuz2001min@mail.ru>
2024-11-18 23:23:12 +0700

    refactor/удаление одного слова :D

--- back/database/model/dbmodel.js
+++ back/database/model/dbmodel.js
@@ -6,7 +6,7 @@ class DbModel {
   return users.create(userData).then(userCreated => {
       return true
     }).catch(err => {
-      console.error('Ошибка создания пользователя. Возможно, пользователь с такими данным email уже существует', err)
+      console.error('Ошибка создания пользователя. Возможно, пользователь с такими email уже существует', err)
       return false
     })
   }
