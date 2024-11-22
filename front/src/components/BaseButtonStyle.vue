868520109ff006946b83503b38666cda29c8bd63
Mubait <kuz2001min@mail.ru>
2024-11-22 23:41:53 +0700

    refactor/убран клик в компоненте кнопка

--- front/src/components/BaseButtonStyle.vue
+++ front/src/components/BaseButtonStyle.vue
@@ -11,7 +11,7 @@ defineProps({
 
 <template>
   <div class="absolute size-full flex flex-col justify-center items-center drop-shadow-xl">
-    <div class="p-0.5 bg-gradient-to-b from-amber-400 to-yellow-950 rounded-lg cursor-pointer" @click="">
+    <div class="p-0.5 bg-gradient-to-b from-amber-400 to-yellow-950 rounded-lg cursor-pointer">
       <div class="p-0.5 bg-gradient-to-b from-yellow-800 to-yellow-950 rounded-lg">
         <div class="p-1 bg-gradient-to-b from-emerald-400 to-emerald-950 rounded-lg">
           <div class="flex flex-col justify-center rounded-lg" 
