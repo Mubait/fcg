c1fb6380c2a4e546ed8ea6473517505bc7d0980b
Mubait <kuz2001min@mail.ru>
2024-11-27 18:40:15 +0700

    refactor/изменения с absolute на relative, убрал size-full, добавил кастомный текст

--- front/src/components/BaseButtonStyle.vue
+++ front/src/components/BaseButtonStyle.vue
@@ -5,18 +5,21 @@ defineProps({
   btnColorHover: String,
   btnTextColor: String,
   btnTextColorHover: String,
+  btnPaddingColor: String,
+  btnText: String,
 })
 
 </script>
 
 <template>
-  <div class="absolute size-full flex flex-col justify-center items-center drop-shadow-xl">
+  <div class="relative flex flex-col justify-center items-center drop-shadow-xl">
     <div class="p-0.5 bg-gradient-to-b from-amber-400 to-yellow-950 rounded-lg cursor-pointer">
       <div class="p-0.5 bg-gradient-to-b from-yellow-800 to-yellow-950 rounded-lg">
-        <div class="p-1 bg-gradient-to-b from-emerald-400 to-emerald-950 rounded-lg">
+        <div class="p-1 rounded-lg"
+        :class="[btnPaddingColor]">
           <div class="flex flex-col justify-center rounded-lg" 
           :class="[btnSize, btnColor, btnColorHover]">
-            <p class="w-full h-full flex flex-col justify-center items-center" :class="[btnTextColor, btnTextColorHover]">Играть Play </p>
+            <p class="w-full h-full flex flex-col justify-center items-center" :class="[btnTextColor, btnTextColorHover]">{{ $t(btnText)}}</p>
           </div>
         </div>
       </div>
