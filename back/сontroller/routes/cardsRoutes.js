87407f57c89498de1bdd754dde387f036bf728df
Mubait <kuz2001min@mail.ru>
2024-12-24 20:40:09 +0700

    feature/добавление проверки на существование токена доступа и его валидности

--- back/сontroller/routes/cardsRoutes.js
+++ back/сontroller/routes/cardsRoutes.js
@@ -5,33 +5,22 @@ const model = require('../../model/model')
 const cardsRouter = express.Router()
 
 cardsRouter.get('/', (req, res) => {
-  model.getCardsJson().then(cards=>{
-    return res.json(cards)
+  try {
+    const accessToken = req.headers['authorization'].split(' ')[1]
+    model.verifyToken(accessToken)
+  } catch(err) {
+    console.error('Ошибка проверки токена или токен отсутствует', err)
+    return res.sendStatus(401)
+  }
+
+  try{
+    model.getCardsJson().then(cards=>{
+      return res.json(cards)
     });
-
-  // const accessToken = req.headers['authorization'].split(' ')[1]
-  // if(!accessToken){
-  //   console.error('Отсутствует токен доступа')
-  //   return res.sendStatus(401)
-  // }
-  
-  // try {
-  //   model.verifyToken(accessToken)
-  // } catch (err) {
-  //   console.error('Ошибка проверки токена', err)
-  //   return res.sendStatus(401)
-  // }
-
-  // try {
-  //   const usernameIsAddedPromise = model.addUsernameToDb(userData, accessToken)
-  //   usernameIsAddedPromise.then(usernameIsAdded => {
-  //     if(usernameIsAdded) return res.sendStatus(200)
-  //     return res.sendStatus(500)
-  //   })
-  // } catch (err) {
-  //   console.error('Ошибка добавления ника в таблицу БД', err)
-  //   return res.sendStatus(500)
-  // }
+  } catch(err) {
+    console.error('Ошибка получения карт с директории', err)
+    return res.sendStatus(500)
+  }
 });
 
 module.exports = cardsRouter;
