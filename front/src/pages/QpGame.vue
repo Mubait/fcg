64d0f820e9e8e69b0457123e32f8bd9184337c6f
Mubait <kuz2001min@mail.ru>
2025-01-06 21:55:13 +0700

    feature/свёрстан выбор героев

--- front/src/pages/QpGame.vue
+++ front/src/pages/QpGame.vue
@@ -10,7 +10,11 @@ let heroesCardsArr = ref()
 
 controllerCards.getHeroesCards()
 .then(heroesCardsArrReturned => {
-  heroesCardsArr.value = heroesCardsArrReturned.orc.concat(heroesCardsArrReturned.elf)
+  heroesCardsArr.value = heroesCardsArrReturned.orc
+  .concat(heroesCardsArrReturned.elf)
+  .concat(heroesCardsArrReturned.werewolf)
+  .concat(heroesCardsArrReturned.undead)
+  .concat(heroesCardsArrReturned.human)
 })
 
 
@@ -28,15 +32,17 @@ else {
     <img class="absolute ml-2 mt-2 object-contain h-1/6 cursor-pointer hover:brightness-50 select-none" src="/logo/logoOrc.png"
     @click="router.push('/main')"/>
     
-    <div class="fixed h-1/2 w-full top-1/2 -translate-y-1/2 p-5
+    <div class="relative h-1/2 w-full top-1/2 -translate-y-1/2 p-5 overflow-x-scroll overflow-y-hidden border-y-2 border-green-400
+    scrollbar-thin scrollbar-thumb-rounded-full scrollbar-thumb-teal-600 active:scrollbar-thumb-teal-300
     bg-gradient-to-r backdrop-blur-sm from-black/80 via-teal-900/80 to-black/80
-    flex space-x-32">
+    flex items-center space-x-32">
       
-      <div v-for="heroCard in heroesCardsArr">
+      <div class="relative size-fit min-w-[15%] transition cursor-pointer
+      hover:brightness-50 hover:scale-105" v-for="heroCard in heroesCardsArr">
         <BaseCard
         :img-url="'http://localhost:3000' + heroCard.imgUrl"
         :mana="heroCard.effectAttributes.mana"
-        :damage="heroCard.effectAttributes.damage"
+        :damage="heroCard.effectAttributes.effect"
         :hp="heroCard.hp"
         :text-size="'text-[12px]'"
         />
