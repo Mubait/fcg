645f858aca692a000a8810f2ab3eaa28a7dccac1
Mubait <kuz2001min@mail.ru>
2025-03-09 20:14:19 +0700

    feature/тест вебсокета

--- back/app.js
+++ back/app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const usersRoutes = require('./сontroller/routes/usersRoutes')
 const cardsRoutes = require('./сontroller/routes/cardsRoutes')
 const path = require('path')
+const http = require('http')
 
 require('dotenv').config()
 
@@ -20,6 +21,27 @@ app
 .use('/users', usersRoutes)
 .use('/cards', cardsRoutes)
 
-app.listen(port, () => {
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Пользователь подключился');
+
+  socket.on('disconnect', () => {
+    console.log('Пользователь отключился');
+  });
+
+  socket.on('send', (message) => {
+    console.log('dasd')
+    socket.broadcast.emit('receive', message);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
