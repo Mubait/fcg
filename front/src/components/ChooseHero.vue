296c61da14cae33df6f208490be9342e1308c736
Mubait <kuz2001min@mail.ru>
2025-07-07 23:16:26 +0700

    fix/исправлена неполадка, в результате которой использовался некорректный URL звука героев

--- front/src/components/ChooseHero.vue
+++ front/src/components/ChooseHero.vue
@@ -35,7 +35,7 @@ const chosenHeroSoundOn = (heroSoundUrl) => {
     <div class="relative size-fit min-w-[15%] transition cursor-pointer border rounded-lg
     hover:scale-105" v-for="(heroCard, index) in heroesCardsArr"
     @mouseover="currentCardHeroIndex = index" @mouseleave="currentCardHeroIndex = null"
-    @click="chosenHeroSoundOn(heroCard.voice[0]), emit('chosenHero', heroCard), isHeroChosen = true">
+    @click="chosenHeroSoundOn(backendUrl+heroCard.voice[0]), emit('chosenHero', heroCard), isHeroChosen = true">
       <div v-if="currentCardHeroIndex == index" class="absolute size-full bg-black/70 rounded-xl 
       animate-[fromBlur_0.5s_ease-in-out_forwards]
       py-5 flex-col z-10">
