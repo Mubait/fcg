455f4953f2ae000005a04f0c1f8858b4a2160508
Mubait <kuz2001min@mail.ru>
2025-07-07 16:20:13 +0700

    refact/изменения в env на front

--- front/src/js/controller/routes/cards.js
+++ front/src/js/controller/routes/cards.js
@@ -2,22 +2,22 @@ import axios from "axios"
 
 class Cards {
   axiosGetCards = () => {
-    return axios.get(`${import.meta.env.BACKEND_URL}/cards`,
+    return axios.get(`${import.meta.env.VITE_BACKEND_URL}/cards`,
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}}
     )
   }
   axiosGetHeroesCards = () => {
-    return axios.get(`${import.meta.env.BACKEND_URL}/cards/getHeroesCards`,
+    return axios.get(`${import.meta.env.VITE_BACKEND_URL}/cards/getHeroesCards`,
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}}
     )
   }
   axiosAddDecks = (decksData) => {
-    return axios.post(`${import.meta.env.BACKEND_URL}/cards/addDecks`, decksData,
+    return axios.post(`${import.meta.env.VITE_BACKEND_URL}/cards/addDecks`, decksData,
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}}
     )
   }
   axiosGetDecks = () => {
-    return axios.get(`${import.meta.env.BACKEND_URL}/cards/getDecks`,
+    return axios.get(`${import.meta.env.VITE_BACKEND_URL}/cards/getDecks`,
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}}
     )
   }
