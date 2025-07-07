9c109cbb2b33bb0584db5dda0538f4765ff9b5b5
Mubait <kuz2001min@mail.ru>
2025-07-07 15:51:11 +0700

    refact/изменение имени свойства в конфиге .env на frontend'е

--- front/src/js/controller/routes/cards.js
+++ front/src/js/controller/routes/cards.js
@@ -2,22 +2,22 @@ import axios from "axios"
 
 class Cards {
   axiosGetCards = () => {
-    return axios.get(`${import.meta.env.VITE_URLSERVER}/cards`,
+    return axios.get(`${import.meta.env.BACKEND_URL}/cards`,
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}}
     )
   }
   axiosGetHeroesCards = () => {
-    return axios.get(`${import.meta.env.VITE_URLSERVER}/cards/getHeroesCards`,
+    return axios.get(`${import.meta.env.BACKEND_URL}/cards/getHeroesCards`,
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}}
     )
   }
   axiosAddDecks = (decksData) => {
-    return axios.post(`${import.meta.env.VITE_URLSERVER}/cards/addDecks`, decksData,
+    return axios.post(`${import.meta.env.BACKEND_URL}/cards/addDecks`, decksData,
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}}
     )
   }
   axiosGetDecks = () => {
-    return axios.get(`${import.meta.env.VITE_URLSERVER}/cards/getDecks`,
+    return axios.get(`${import.meta.env.BACKEND_URL}/cards/getDecks`,
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}}
     )
   }
--- front/src/js/controller/routes/users.js
+++ front/src/js/controller/routes/users.js
@@ -2,22 +2,22 @@ import axios from "axios"
 
 class Users {
   axiosPostUserSignin = (userData) => {
-    return axios.post(`${import.meta.env.VITE_URLSERVER}/users/signin`, userData /*{ headers: { Authorization: `Bearer justtoken`}}*/)
+    return axios.post(`${import.meta.env.BACKEND_URL}/users/signin`, userData )
   }
   axiosPostUserSignup = (userData) => {
-    return axios.post(`${import.meta.env.VITE_URLSERVER}/users/signup`, userData)
+    return axios.post(`${import.meta.env.BACKEND_URL}/users/signup`, userData)
   }
   axiosPostUserAddnick = (userData) => {
-    return axios.post(`${import.meta.env.VITE_URLSERVER}/users/addnick`, userData, 
+    return axios.post(`${import.meta.env.BACKEND_URL}/users/addnick`, userData, 
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}})
   }
   axiosGetUserCheckNick = () => {
-    return axios.get(`${import.meta.env.VITE_URLSERVER}/users/checkNickname`,
+    return axios.get(`${import.meta.env.BACKEND_URL}/users/checkNickname`,
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}}
     )
   }
   axiosGetUserNick = () => {
-    return axios.get(`${import.meta.env.VITE_URLSERVER}/users/getNick`,
+    return axios.get(`${import.meta.env.BACKEND_URL}/users/getNick`,
       { headers: { Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`}}
     )
   }
